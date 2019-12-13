a936f861ebcf1ead16a7477c5b20f67098de7d64
swap-file <bigmarkslp@gmail.com>
2019-12-12 21:15:09 -0600

    Update for 1.14
    Name fixed, and Pi4 Binary included

--- gstaasink.c
+++ gstaasink.c
@@ -593,6 +593,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    aalib,
+    aasink,
     "ASCII Art video sink & filter",
     plugin_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN);
